aebf2338ece5785fe7056384479da9f2b8866c48
cnDengyu <littlesunny2001@163.com>
2023-09-03 16:10:51 +0800

    Fix and Test the experiment data retrain script

--- gym_pybullet_drones/examples/experiment_retrain.py
+++ gym_pybullet_drones/examples/experiment_retrain.py
@@ -17,14 +17,14 @@ from stable_baselines3.common.policies import ActorCriticPolicy
 
 
 class RetrainEnv(gym.Env):
-    def __init__(self, data_file_name: str = "data.txt"):
+    def __init__(self, data: np.ndarray):
         
         # 定义动作空间
         #Box(low=np.array([-1.0, -2.0]), high=np.array([2.0, 4.0]), dtype=np.float32)
         #Box(2,)
         self.action_space = spaces.Box(
-            low=np.array([0.1,0.1,0.1]),
-            high=np.array([5,5,5]),
+            low=np.array([0.1,0.1,0.1,0.1,0.1,0.1]),
+            high=np.array([10,10,10,10,10,10]),
             dtype=np.float32
             )
         # self.self.PYB_CLIENT = p.connect(p.GUI if self._render else p.DIRECT)
@@ -39,27 +39,75 @@ class RetrainEnv(gym.Env):
         
         self.step_num: int = 0
 
-        self.data: np.ndarray = np.loadtxt(data_file_name)
+        self.data: np.ndarray = data
         self.data_length: int = self.data.shape[0]
 
     def step (self, 
               act: Union[None, np.ndarray] = None) -> Tuple[Any, Any, Any, Any]:
-        states = self.data[self.step_num, :]
+        states = self.data[act, 0:20]
         rewards = 0
         done = False
-        info = None
+        info = {}
         return states, rewards, done, info
     
-    def get_train_data(self):
-        return self.data[:, 7:10]
+    def reset(self):
+        states = self.data[0, 0:20]
+        rewards = 0
+        done = False
+        info = {}
+        return states
+    
     
 class RetrainPPO(PPO):
     def __init__(self, 
                  policy: Union[str, Type[ActorCriticPolicy]],
                  env: Union[GymEnv, str],
-                 data: np.ndarray):
-        super(RetrainPPO, self).__init__(policy, env)
-        self.experiment_data = data.to(self.device)
+                 data: np.ndarray,
+                 learning_rate: Union[float, Schedule] = 3e-4,
+                 n_steps: int = 2048,
+                 batch_size: int = 64,
+                 n_epochs: int = 10,
+                 gamma: float = 0.99,
+                 gae_lambda: float = 0.95,
+                 clip_range: Union[float, Schedule] = 0.2,
+                 clip_range_vf: Union[None, float, Schedule] = None,
+                 normalize_advantage: bool = True,
+                 ent_coef: float = 0.0,
+                 vf_coef: float = 0.5,
+                 max_grad_norm: float = 0.5,
+                 use_sde: bool = False,
+                 sde_sample_freq: int = -1,
+                 target_kl: Optional[float] = None,
+                 tensorboard_log: Optional[str] = None,
+                 create_eval_env: bool = False,
+                 policy_kwargs: Optional[Dict[str, Any]] = None,
+                 verbose: int = 0,
+                 seed: Optional[int] = None,
+                 device: Union[th.device, str] = "auto",
+                 _init_setup_model: bool = True):
+        super(RetrainPPO, self).__init__(policy, env, 
+                                         learning_rate, 
+                                         n_steps, 
+                                         batch_size, 
+                                         n_epochs, 
+                                         gamma, 
+                                         gae_lambda, 
+                                         clip_range, 
+                                         clip_range_vf,
+                                         normalize_advantage,
+                                         ent_coef,
+                                         vf_coef,
+                                         max_grad_norm,
+                                         use_sde,
+                                         sde_sample_freq,
+                                         target_kl,
+                                         tensorboard_log,
+                                         create_eval_env,
+                                         policy_kwargs,
+                                         verbose,
+                                         seed,
+                                         device)
+        self.experiment_data = th.from_numpy(data).to(self.device)
 
     def collect_rollouts(
         self,
@@ -103,9 +151,9 @@ class RetrainPPO(PPO):
                 obs_tensor = obs_as_tensor(self._last_obs, self.device)
                 # actions, values, log_probs = self.policy(obs_tensor)
                 # Get actions, values, log_probs from data file
-                actions = self.experiment_data[self.num_timesteps, 0]
-                values = self.experiment_data[self.num_timesteps, 1]
-                log_probs = self.experiment_data[self.num_timesteps, 2]
+                actions = self.experiment_data[self.num_timesteps, 2:8]
+                values = self.experiment_data[self.num_timesteps, 0]
+                log_probs = self.experiment_data[self.num_timesteps, 1]
             actions = actions.cpu().numpy()
 
             # Rescale and perform action
@@ -114,7 +162,8 @@ class RetrainPPO(PPO):
             if isinstance(self.action_space, gym.spaces.Box):
                 clipped_actions = np.clip(actions, self.action_space.low, self.action_space.high)
 
-            new_obs, rewards, dones, infos = env.step(clipped_actions)
+            # new_obs, rewards, dones, infos = env.step(clipped_actions)
+            new_obs, rewards, dones, infos = env.step(np.array([self.num_timesteps]))
 
             self.num_timesteps += env.num_envs
 
@@ -159,9 +208,17 @@ class RetrainPPO(PPO):
 
     
 if __name__ == '__main__':
-    env = RetrainEnv("data.txt")
-    train_data = env.get_train_data()
-    model = RetrainPPO("MlpPolicy", env, train_data, verbose=1, tensorboard_log="./ppo/")
-    model.learn(total_timesteps=10000000)
-    model.save("ppo_cartpole4")
+    data = np.vstack((np.loadtxt("ICRA_data.txt"),
+                      np.loadtxt("ICRA_data1.txt"),
+                      np.loadtxt("ICRA_data2.txt"),
+                      np.loadtxt("ICRA_data3.txt"),
+                      np.loadtxt("ICRA_data4.txt"),
+                      np.loadtxt("ICRA_data5.txt"),
+                      np.loadtxt("ICRA_data6.txt"),
+                      np.loadtxt("ICRA_data7.txt"),
+                      np.loadtxt("ICRA_data8.txt")))
+    env = RetrainEnv(data)
+    model = RetrainPPO("MlpPolicy", env, data[:, 20:28], verbose=1, tensorboard_log="./ppo/")
+    model.learn(total_timesteps=2048)
+    model.save("ppo_retrain")
     
